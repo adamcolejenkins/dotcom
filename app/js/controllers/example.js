dd6e62fbb115d572530f32568b2fc0969168ba20
bstriker <strikeforcezero@gmail.com>
2015-11-27 13:40:21 -0500

    Add fullpage.js example

--- app/js/controllers/example.js
+++ app/js/controllers/example.js
@@ -8,6 +8,14 @@ function ExampleCtrl() {
   vm.title = 'AngularJS, Gulp, and Browserify! Written with keyboards and love!';
   vm.number = 1234;
 
+
+  vm.mainOptions = {
+    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE'],
+    navigation: true,
+    navigationPosition: 'right',
+    scrollingSpeed: 1000
+  }
+
 }
 
 export default {
